4d059becde10e824731ea79d0c4a9e0c0a67625b
NinjaBitroom <gabrielbitercourt@gmail.com>
2024-09-13 10:12:16 -0400

    Disponibilize o Dio globalmente

--- lib/controllers/nyt_api_controller.dart
+++ lib/controllers/nyt_api_controller.dart
@@ -1,18 +1,9 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
 import 'package:nyt_best_sellers/models/book_overview_model.dart';
 
 class NytApiController extends GetMaterialController {
-  final _dio = Dio(
-    BaseOptions(
-      baseUrl: 'https://api.nytimes.com/svc/books/v3',
-      queryParameters: {
-        'api-key': dotenv.env['API_KEY'],
-      },
-    ),
-  );
   var books = [].obs;
 
   @override
@@ -25,7 +16,7 @@ class NytApiController extends GetMaterialController {
     try {
       books.clear();
       final bestSellers = [];
-      final response = await _dio.get('/lists/overview.json');
+      final response = await Get.find<Dio>().get('/lists/overview.json');
       for (var result in response.data['results']['lists']) {
         for (var book in result['books']) {
           final bookOverview = BookOverviewModel(
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
@@ -7,6 +8,16 @@ import 'package:nyt_best_sellers/controllers/nyt_api_controller.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
+  Get.put(
+    Dio(
+      BaseOptions(
+        baseUrl: 'https://api.nytimes.com/svc/books/v3',
+        queryParameters: {
+          'api-key': dotenv.env['API_KEY'],
+        },
+      ),
+    ),
+  );
   Get.put(NytApiController());
   runApp(const App());
 }
